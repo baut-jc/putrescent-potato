4c319ee6de777bb18e7950982ebbd0f0db499ed9
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-10 16:46:40 -0700

    Add styling and classNames to movieDetails files

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './MovieDetail.css'
-import fetchData from '../../api'
-import cleanData from '../../utilities'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./MovieDetail.css";
+import fetchData from "../../api";
+import cleanData from "../../utilities";
+import PropTypes from "prop-types";
 
 class MovieDetail extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      movieData: {}
-    }
+      movieData: {},
+    };
   }
 
   componentDidMount() {
-    fetchData(this.props.id)
-      .then(data => {
-        const cleanedData = cleanData(data.movie)
-        this.setState({movieData: cleanedData})
-      })
+    fetchData(this.props.id).then((data) => {
+      const cleanedData = cleanData(data.movie);
+      this.setState({ movieData: cleanedData });
+    });
   }
 
   render() {
-    return(
-      <div className='movie-detail' style={{backgroundImage: `url(${this.state.movieData.backdrop_path})`}}>
-        <div className='poster-section'>
+    return (
+      <div
+        className="movie-detail"
+        style={{
+          backgroundImage: `url(${this.state.movieData.backdrop_path})`,
+        }}
+      >
+        <div className="poster-section">
           <h2>{this.state.movieData.title}</h2>
-          <img className='poster' src={this.state.movieData.poster_path}/>
+          <img className="poster" src={this.state.movieData.poster_path} />
+          <div className='text-section'>
+            <p>{this.state.movieData.release_date}</p>
+            <p>{this.state.movieData.tagline}</p>
+            <p>{this.state.movieData.runtime}</p>
+            <p>{this.state.movieData.genres}</p>
+            <p>{this.state.movieData.budget}</p>
+            <p>{this.state.movieData.revenue}</p>
+            <p>{this.state.movieData.overview}</p>
+          </div>
         </div>
-        <div>
-          <p>{this.state.movieData.release_date}</p>
-          <p>{this.state.movieData.tagline}</p>
-          <p>{this.state.movieData.runtime}</p>
-          <p>{this.state.movieData.genres}</p>
-          <p>{this.state.movieData.budget}</p>
-          <p>{this.state.movieData.revenue}</p>
-          <p>{this.state.movieData.overview}</p>
-        </div>
-        <Link to='/'>
+
+        <Link to="/">
           <button>Home</button>
         </Link>
-       </div>
-    )
+      </div>
+    );
   }
-
 }
 
-export default MovieDetail
+export default MovieDetail;
 
 MovieDetail.propTypes = {
-  id: PropTypes.number.isRequired
-}
+  id: PropTypes.number.isRequired,
+};
